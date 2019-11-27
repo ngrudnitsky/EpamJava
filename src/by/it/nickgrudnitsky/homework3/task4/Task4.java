944eac7db5371547a2da71e5ebec0aeba26e6ca8
Nick Grudnitsky <grud97@gmail.com>
2019-11-27 11:08:03 +0300

    Homework 3

--- src/by/it/nickgrudnitsky/homework3/task4/Task4.java
+++ src/by/it/nickgrudnitsky/homework3/task4/Task4.java
@@ -1,4 +1,27 @@
 package by.it.nickgrudnitsky.homework3.task4;
 
+import static by.it.nickgrudnitsky.homework3.util.MatrixGeneration.generateFloatMatrix;
+import static by.it.nickgrudnitsky.homework3.util.MatrixGeneration.printMatrix;
+
 public class Task4 {
+    public static void main(String[] args) {
+        double[][] matrix = generateFloatMatrix(3, 4, -10, 10);
+        printMatrix(matrix);
+        printMatrix(transposeMatrix(matrix));
+    }
+
+    //Задана матрица размера N x M. Необходимо выполнить следующие действия:
+    //− транспонировать матрицу (при решении данного задания не рекомендуется задействовать дополнительную память).
+
+    private static double[][] transposeMatrix(double[][] matrix) {
+        double[][] transposedMatrix = new double[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        return transposedMatrix;
+    }
+
+
 }
